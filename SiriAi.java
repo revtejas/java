03c69a5d07d6993a72dedc7dedaff95a64e41b6f
Tejas Kumar <96368690+revtejas@users.noreply.github.com>
2022-01-10 17:33:40 +0530

    Add files via upload

---
+++ SiriAi.java
@@ -0,0 +1,17 @@
+
+public class SiriAi {
+String welcomeNote = "Hello, I'm Siri";
+	
+	public void sayTime() {
+		System.out.println("The time is 6:15 pm");
+	}
+	
+	public void playMusic() {
+		System.out.println("Playing your fav music!");
+	}
+	public void playMusic(String name) {
+		System.out.println("Playing :" + name);
+	}
+	
+
+}
---
+++ googleAi.java
@@ -0,0 +1,10 @@
+
+public class googleAi {
+	
+	String welcomeNote = "Hello, I'm google";
+	
+	public void sayTime() {
+		System.out.println("The time is 6:15 pm");
+	}
+
+}
